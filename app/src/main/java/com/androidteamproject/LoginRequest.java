dfcfd9b8f291a8a5e1046200616167880df27fd3
CJH <jhchie93@gmail.com>
2022-02-28 09:32:52 +0900

    login mid-commit

---
+++ app/src/main/java/com/androidteamproject/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.androidteamproject;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+//    서버 URL 설정
+    final static private String URL = "http://13.209.76.14:8080/login";
+
+    private Map<String, String> map;
+
+    public LoginRequest(String userId, String userPw, Response.Listener<String> listener, Response.ErrorListener responseErrorListener) {
+        super(Method.POST, URL, listener, responseErrorListener);
+
+        map = new HashMap<>();
+        map.put("userId", userId);
+        map.put("userPw", userPw);
+    }
+
+    @Nullable
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
