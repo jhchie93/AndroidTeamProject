1c0071c0fa95dd3dbbd73b7f54d246d787dfa0ca
CJH <jhchie93@gmail.com>
2022-02-28 09:29:14 +0900

    login mid-commit

--- app/src/main/java/com/androidteamproject/MainActivity.java
+++ app/src/main/java/com/androidteamproject/MainActivity.java
@@ -1,14 +1,99 @@
 package com.androidteamproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText editId, editPw;
+    Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editId = findViewById(R.id.edit_id);
+        editPw = findViewById(R.id.edit_pw);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginProcess();
+            }
+        });
+
     }
+
+    public void loginProcess() {
+        String id = editId.getText().toString();
+        String pw = editPw.getText().toString();
+
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    if (success) {
+                        String userId = jsonObject.getString("userId");
+                        String userPw = jsonObject.getString("userPw");
+
+                        Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+
+                               /* Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("userId", userId);
+                                intent.putExtra("userPw", userPw);
+                                startActivity(intent);
+                                */
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Response.ErrorListener responseErrorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                System.out.println(error.getMessage());
+                Log.i("에러 : ", error.getMessage());
+            }
+        };
+
+        LoginRequest loginRequest = new LoginRequest(id, pw, responseListener, responseErrorListener);
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        queue.add(loginRequest);
+
+    }
+
 }
